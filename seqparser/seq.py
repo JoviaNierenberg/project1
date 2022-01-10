76879c305db3baf3d3067122ffef12bcfed9e442
Jovia Nierenberg <jovia.nierenberg@ucsf.edu>
2022-01-09 16:34:01 -0800

    wrote transcribe and reverse_transcribe functions, start jupyter notebook for figuring things out

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,10 +6,20 @@ def transcribe(seq: str) -> str:
     transcribes DNA to RNA by generating
     the complement sequence with T -> U replacement
     """
-
+    # replacement function
+    def replace_all(text, dictionary):
+        for key, val in dictionary.items():
+            text = text.replace(key, val)
+        return text
+    # create dictionary with text and replacement text, use interim letters P & Q so that C and G are properly replaced
+    d = { "A":"U", "T":"A", "G":"P", "C":"Q", "P:C"}
+    # return result of running replace_all on the sequence and the dictionary
+    return replace_all(seq, d)
 
 def reverse_transcribe(seq: str) -> str:
     """
     transcribes DNA to RNA then reverses
     the strand
     """
+    transcribed = transcribe(seq)
+    return transcribed[::-1] # use slicing to reverse string
